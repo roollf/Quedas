91fd43f2add7b6e6f28d04f9ea95660928119c4f
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-05-22 17:57:39 -0300

    :bug: fix: update ManagementComponent to use async data binding for card data and improve template structure

--- src/app/pages/management/management.component.ts
+++ src/app/pages/management/management.component.ts
@@ -1,12 +1,31 @@
+// angular
 import { Component } from '@angular/core';
+
+// components
 import { LayoutComponent } from '../../components/layout/layout.component';
+
+// services
+import { DashboardService } from '../../services/api/dashboard.service';
+
+// pipes
+import { AsyncPipe } from '@angular/common';
+
+// rxjs
+import { Observable } from 'rxjs';
+
+// composable
+import { State, withLoadingAndError } from '../../@shared/composables/api/use-http';
 
 @Component({
   selector: 'app-management',
-  imports: [LayoutComponent],
+  imports: [LayoutComponent, AsyncPipe],
   templateUrl: './management.component.html',
-  styleUrl: './management.component.css'
+  styleUrl: './management.component.css',
 })
 export class ManagementComponent {
+  cardData$: Observable<State<[]>>;
 
+  constructor(private dashboardService: DashboardService) {
+    this.cardData$ = withLoadingAndError<[]>(this.dashboardService.getDashboard());
+  }
 }
