eb0e32f5b08f946080f555f96fe371975e96024a
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-05-22 17:57:31 -0300

    :bug: fix: refactor DashboardComponent to use async data binding for patient cards and update CSS grid layout

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,29 @@
+// angular
 import { Component } from '@angular/core';
+
+// components
 import { LayoutComponent } from '../../components/layout/layout.component';
 import { PatientCardComponent } from '../../components/patient-card/patient-card.component';
 
+// services
+import { DashboardService } from '../../services/api/dashboard.service';
+
+// pipes
+import { AsyncPipe } from '@angular/common';
+
+// rxjs
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [LayoutComponent, PatientCardComponent],
+  imports: [LayoutComponent, PatientCardComponent, AsyncPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  cardData$: Observable<[]>;
+
+  constructor(private dashboardService: DashboardService) {
+    this.cardData$ = this.dashboardService.getDashboard();
+  }
+}
