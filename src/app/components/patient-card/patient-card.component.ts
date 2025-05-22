c5fe672868eeeea82e70e5609ad8d0bd99b24853
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-05-22 17:57:11 -0300

    :bug: fix: refactor PatientCardComponent to use reactive state management and improve alert handling

--- src/app/components/patient-card/patient-card.component.ts
+++ src/app/components/patient-card/patient-card.component.ts
@@ -1,18 +1,44 @@
-import { Component, Input } from '@angular/core';
+// angular
+import { Component, effect, input, OnInit, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+
+// services
+import { AlarmService } from '../../services/utils/alarm.service';
+
+// components
+import { PatientStatusComponent } from './patient-status/patient-status.component';
 
 @Component({
   selector: 'app-patient-card',
-  imports: [],
+  imports: [NgClass, PatientStatusComponent],
   templateUrl: './patient-card.component.html',
   styleUrl: './patient-card.component.css',
 })
-export class PatientCardComponent {
-  @Input() ambientName: string = '';
-  @Input() patientName: string = '';
-  @Input() status: boolean = false;
-  @Input() pause: boolean = false;
-  @Input() power: boolean = false;
-  @Input() alarm: string = '';
+export class PatientCardComponent implements OnInit {
+  patientData = input<any>({});
+  alertClasses: Record<string, boolean> = {};
+  alert = signal<{ risk: number; alert: boolean; title: string }>({
+    risk: 1,
+    alert: false,
+    title: '',
+  });
 
-  constructor() {}
+  constructor(private alarmService: AlarmService) {}
+
+  ngOnInit(): void {
+    this._evaluateAlarmValueEffect;
+    this._setAlarmClassesEffect;
+  }
+
+  private _evaluateAlarmValueEffect = effect(() => {
+    const data = this.patientData();
+    this.alert.set(this.alarmService.evaluateAlarm(data));
+  });
+
+  private _setAlarmClassesEffect = effect(() => {
+    this.alertClasses = {
+      'fall-risk-alert': this.alert().risk === 2 && this.alert().alert,
+      'fall-alert': this.alert().risk === 3 && this.alert().alert,
+    };
+  });
 }
