ddae5a481b9105b29e1486a6a7a1454b1a5bff7f
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-05-27 17:37:09 -0300

    :sparkles: feat: update dashboard component to use PatientCardContainer and improve async data handling

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 // components
 import { LayoutComponent } from '../../components/layout/layout.component';
-import { PatientCardComponent } from '../../components/patient-card/patient-card.component';
+import { PatientCardContainer } from '../../containers/patient-card/patient-card.container';
 
 // services
 import { DashboardService } from '../../services/api/dashboard.service';
@@ -14,16 +14,22 @@ import { AsyncPipe } from '@angular/common';
 // rxjs
 import { Observable } from 'rxjs';
 
+// composable
+import { toAsyncState } from '../../@shared/composables/api/to-async-state';
+
+// models
+import * as Models from '../../@shared/models';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [LayoutComponent, PatientCardComponent, AsyncPipe],
+  imports: [LayoutComponent, PatientCardContainer, AsyncPipe],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent {
-  cardData$: Observable<[]>;
+  cardData$: Observable<Models.AsyncState<[]>>;
 
   constructor(private dashboardService: DashboardService) {
-    this.cardData$ = this.dashboardService.getDashboard();
+    this.cardData$ = toAsyncState<[]>(this.dashboardService.getDashboard());
   }
 }
