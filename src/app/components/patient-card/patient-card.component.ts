852272c3cc5200cc2a64454e229f253203aef662
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-05-27 17:37:30 -0300

    :sparkles: feat: refactor PatientCard component to use ng-content for dynamic content and create PatientCardContainer for improved structure

--- src/app/components/patient-card/patient-card.component.ts
+++ src/app/components/patient-card/patient-card.component.ts
@@ -1,44 +1,14 @@
 // angular
-import { Component, effect, input, OnInit, signal } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NgClass } from '@angular/common';
-
-// services
-import { AlarmService } from '../../services/utils/alarm.service';
-
-// components
-import { PatientStatusComponent } from './patient-status/patient-status.component';
 
 @Component({
   selector: 'app-patient-card',
-  imports: [NgClass, PatientStatusComponent],
+  imports: [NgClass],
   templateUrl: './patient-card.component.html',
   styleUrl: './patient-card.component.css',
 })
-export class PatientCardComponent implements OnInit {
-  patientData = input<any>({});
-  alertClasses: Record<string, boolean> = {};
-  alert = signal<{ risk: number; alert: boolean; title: string }>({
-    risk: 1,
-    alert: false,
-    title: '',
-  });
-
-  constructor(private alarmService: AlarmService) {}
-
-  ngOnInit(): void {
-    this._evaluateAlarmValueEffect;
-    this._setAlarmClassesEffect;
-  }
-
-  private _evaluateAlarmValueEffect = effect(() => {
-    const data = this.patientData();
-    this.alert.set(this.alarmService.evaluateAlarm(data));
-  });
-
-  private _setAlarmClassesEffect = effect(() => {
-    this.alertClasses = {
-      'fall-risk-alert': this.alert().risk === 2 && this.alert().alert,
-      'fall-alert': this.alert().risk === 3 && this.alert().alert,
-    };
-  });
+export class PatientCardComponent {
+  @Input() alertClasses: Record<string, boolean> = {};
+  @Input() isAlertActive: boolean = false;
 }
