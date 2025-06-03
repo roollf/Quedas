cfade525e96599ae529afe9869588b5f7bfae3c1
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-06-03 17:45:43 -0300

    :sparkles: feat: reorganize imports and ensure TanStack Query provider is included in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,22 @@
+// angular
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
+// routes
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+
+// libs
+import {
+  provideTanStackQuery,
+  QueryClient,
+} from '@tanstack/angular-query-experimental';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(withFetch()),
+    provideTanStackQuery(new QueryClient()),
   ],
 };
