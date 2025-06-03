295f45259f357542489b3de0757be5c2b7f3a850
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-06-03 17:46:09 -0300

    :recycle: refactor: enhance logging in toAsyncState function to display received data

--- src/app/@shared/composables/api/to-async-state.ts
+++ src/app/@shared/composables/api/to-async-state.ts
@@ -8,7 +8,10 @@ export function toAsyncState<T>(
   source$: Observable<T>
 ): Observable<Models.AsyncState<T>> {
   return source$.pipe(
-    map((data) => ({ loading: false, data })),
+    map((data) => {
+      console.log('Dados recebidos', data);
+      return { loading: false, data };
+    }),
     startWith({ loading: true } as Models.AsyncState<T>),
     catchError((error) => of({ loading: false, error }))
   );
