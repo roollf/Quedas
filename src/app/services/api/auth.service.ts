fb2f595ac51107d0d5700b8c5fb4d8a7abeeb36f
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-06-05 11:49:04 -0300

    :sparkles: feat: add AuthService and UserService for API integration

---
+++ src/app/services/api/auth.service.ts
@@ -0,0 +1,21 @@
+// angular
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+// rxjs
+import { Observable } from 'rxjs';
+
+// environments
+import { environment } from '../../../environments/environment';
+
+// models
+import * as Models from '../../@shared/models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private readonly _json_server_url = environment.JSON_SERVER_ENDPOINT;
+
+  constructor(private httpClient: HttpClient) {}
+}
